4e9fb1cb8512d48d40f3013e47718284ddbb13b8
ocaananda <ocaananda09@gmail.com>
2024-07-24 18:46:30 +0600

    Add files via upload

---
+++ kalkulator/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace kalkulator
+{
+    public partial class Form1 : Form
+    {
+        private object screen;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void op_pres(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+}
